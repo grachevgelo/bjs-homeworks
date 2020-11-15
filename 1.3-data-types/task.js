ad094be50d0bae697c71e870b7f2a64fb55ecdec
Oleg Grachev <grachev.gelo@gmail.com>
2020-11-15 16:20:13 +0300

    Home work

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,22 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let persentP = percent / 100; // округление до дроби
+        date2 = new Date(); // дата сегодняшнего дня
+        creditBody = amount - contribution; //тело кредита
+        payMounthMsec = date - date2; // просчет разности в месяце (на выходе получаем миллисекунды, как получить месяц?)
+        totalAmount = creditBody * (persentP + persentP / ((Math.pow(1 + persentP), payMounthMsec) - 1)); // Платеж=S*(P+P/(((1+P)^n)-1)), где: S - тело кредита, P - 1/12 процентной ставки (от 0 до 1), n - количество месяцев ^ - возведение в степень
+return totalAmount.toFixed(2); // ответ округлен, но из-за месяца выдает некорректный ответ
+
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
-}
+let greeting = `Привет, мир! Меня зовут ${name}.`;
+ if (name === "") {
+  let name = "Аноним";
+      greeting = `Привет, мир! Меня зовут ${name}.`;
+   return greeting;
+} else {
+    return greeting;
+  }
+};
