0fba6e65dcdf82e4add1f5dee94f3f3084158522
Oleg Grachev <grachev.gelo@gmail.com>
2020-11-16 22:49:38 +0300

    DONE!

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,40 +1,37 @@
 "use strict";
 function getResult(a,b,c){
   let d = Math.pow(b, 2) - 4 * a * c;
-      x = [];
-if (d == 0) {
+  let x = [];
+if (d === 0) {
   x[0] = -b / (2 * a);
-   return x;
   } else if (d < 0) {
-    return 0;
+    x = [0];
      } else if (d > 0) {
       x[0] = (-b + Math.sqrt(d)) / (2 * a);
       x[1] = (-b - Math.sqrt(d)) / (2 * a);
-      return x;
     }
+    return x;
 };
 
 function getAverageMark(marks){
-  sumMarks = 0;
-  if (marks.length == 0) {
-    averageMark = marks.length;
-     return averageMark;
-  } else if (marks.length > 5) {
-       console.log("Оценок больше 5");
-     for (let i = 0; i < marks.length; i++) {
-       let marksSlice = marks.splice(5, 1);
-           sumMarks += marks[i];
-      };
-       let averageMark = sumMarks / marks.length;
-        return averageMark;
+  let sumMarks = 0;
+  if (marks.length > 5) {
+    console.log("Оценок больше 5");
+  } else if (marks.length === 0) {
+     averageMark = 0;
     };
-}
+  marks.splice(5, 99);
+  for (let i = 0; i < marks.length; i++) {
+    sumMarks += marks[i];
+  };
+ let averageMark = sumMarks / marks.length;
+  return averageMark;
+};
 
 function askDrink(name,dateOfBirthday){
-    let date = new Date().getFullYear();
-        failMessage = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
-        confirmMessage = `Не желаете ли олд-фэшн, ${name}?`;
-        newDate = date - dateOfBirthday.getFullYear();
+    let failMessage = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    let confirmMessage = `Не желаете ли олд-фэшн, ${name}?`;
+    let newDate = new Date().getFullYear() - dateOfBirthday.getFullYear();
     if (newDate < 18) {
       return failMessage;
     } else {
