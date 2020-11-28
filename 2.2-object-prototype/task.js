5d864054c9d2c781a9fd31bbf68930a2cbab8fe5
Oleg Grachev <grachev.gelo@gmail.com>
2020-11-28 16:22:17 +0300

    DONE!

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,30 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+    let abc = this.toLowerCase().replace(/\s/g, '').split("");
+    return abc.join("") == abc.reverse().join("");
+   };
+   
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let sumMarks = 0;
+    if (marks.length == 0) {
+        return 0;
+    };
+    for (let i = 0; i < marks.length; i++) {
+     sumMarks += marks[i];
+    };
+    let average = sumMarks / marks.length,
+        roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+    let now = +new Date();
+        birthday = +new Date(birthday);
+    let diff = now - birthday,
+        age = diff / 31536000000;
+    if (age >= 18) {
+     return true;
+    } else {
+       return false;
+    };
+};
